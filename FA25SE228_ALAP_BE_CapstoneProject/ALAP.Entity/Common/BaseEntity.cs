a5a2283cb86c233c2561aaf668bfcc09ee16bde8
dothephi <phidtse161229@fpt.edu.vn>
2025-12-13 20:32:00 +0700

    "Create base entity"

---
+++ FA25SE228_ALAP_BE_CapstoneProject/ALAP.Entity/Common/BaseEntity.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Base.Common;
+
+namespace ALAP.Entity.Common
+{
+    public class BaseEntity
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public long Id { get; set; }
+
+        public DateTime CreatedAt { get; set; } = Utils.GetCurrentVNTime();
+
+        public DateTime UpdatedAt { get; set; }
+    }
+}
