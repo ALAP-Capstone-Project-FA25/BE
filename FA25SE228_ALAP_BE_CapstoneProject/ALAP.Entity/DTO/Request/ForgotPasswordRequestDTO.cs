a9b7a548dc356ed4e2eac2695f8cf90dbdde8488
longND03 <longndse170223@fpt.edu.vn>
2025-12-25 14:48:42 +0700

    Add Auth + Forgot Password

---
+++ FA25SE228_ALAP_BE_CapstoneProject/ALAP.Entity/DTO/Request/ForgotPasswordRequestDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ALAP.Entity.DTO.Request
+{
+    public class ForgotPasswordRequestDTO
+    {
+        [Required(ErrorMessage = "The email field is required.")]
+        [EmailAddress(ErrorMessage = "The email field must be a valid email address.")]
+        public string Email { get; set; }
+    }
+}
